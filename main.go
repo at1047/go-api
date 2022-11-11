f8c351599004be48c2e2b2d42833db001217275e
at1047 <andrewhytai@gmail.com>
2022-11-11 14:47:58 -0500

    Added Gin API

--- main.go
+++ main.go
@@ -1,143 +1,100 @@
 package main
+
 import (
-	"context"
-	_"encoding/json"
-    "strconv"
-	"fmt"
+    _"net/http"
+    "github.com/gin-gonic/gin"
+    _"errors"
+    _"strconv"
+	_"context"
+    _"fmt"
+    _"log"
+    "go-recipes/lib"
+	_"go.mongodb.org/mongo-driver/bson"
+	_"go.mongodb.org/mongo-driver/mongo"
     "bufio"
+    "os"
     "strings"
-	"log"
-	"os"
-	"github.com/joho/godotenv"
-	// "go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// type Todo struct {
+//     ID          int     `json:"id"`
+//     Item        string  `json:"item"`
+//     Completed   bool    `json:"completed"`
+// }
+//
+// var todos = []Todo {
+//     {ID: 1, Item: "Clean Room", Completed: false},
+//     {ID: 2, Item: "Read Book", Completed: false},
+//     {ID: 3, Item: "Record Video", Completed: false},
+// }
+//
 
-// type Trainer struct {
-//     Name string
-//     Age  int
-//     City string
+// func addTodo(context *gin.Context) {
+//     var newTodo Todo
+//
+//     if err := context.BindJSON(&newTodo); err != nil {
+//         return
+//     }
+//
+//     todos = append(todos, newTodo)
+//     context.IndentedJSON(http.StatusCreated, newTodo)
+// }
+//
+// func getTodoById(id int) (*Todo, error) {
+//     for i, t := range todos {
+//         if t.ID == id {
+//             return &todos[i], nil
+//         }
+//     }
+//
+//     return nil, errors.New("Todo not found")
+// }
+//
+// func getTodo(context *gin.Context) {
+//     id, err := strconv.Atoi(context.Param("id"))
+//
+//
+//
+//     todo, err := getTodoById(id)
+//
+//     if err != nil {
+//         context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+//         return
+//     }
+//
+//     context.IndentedJSON(http.StatusOK, todo)
 // }
 
-type Ingredient struct {
-    Name string
-    Quantity float64
-    Unit string
-}
 
-type Recipe struct {
-    Name string
-    Ingredients []Ingredient
-    Notes string
-}
-
-
-func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-	uri := fmt.Sprintf("mongodb+srv://%s:%s@cluster0.5i9vwnq.mongodb.net/?retryWrites=true&w=majority", os.Getenv("USERNAME"), os.Getenv("PASSWORD"))
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-	coll := client.Database("recipes").Collection("recipes")
+func readInput() string {
     reader := bufio.NewReader(os.Stdin)
-
-    readInput := func() string {
-        text, _ := reader.ReadString('\n')
-        text = strings.Replace(text, "\n", "", -1)
-        return text
-    }
-
-    fmt.Println("Simple Shell")
-    fmt.Println("---------------------")
-
-    fmt.Print("Recipe name: ")
-    recipeName := readInput()
-
-    var ingredients []Ingredient
-
-    for {
-        fmt.Print("Add ingredient: ")
-        ingredientName := readInput()
-
-        if strings.Compare("exit", ingredientName) == 0 {
-            fmt.Println("Exiting...")
-            break
-        }
-
-        fmt.Print("Quantity: ")
-        ingredientQuantity, _ := strconv.ParseFloat(readInput(), 32)
-
-        fmt.Print("Unit: ")
-        ingredientUnit := readInput()
-
-        ingredient := Ingredient {
-            Name: ingredientName,
-            Quantity: ingredientQuantity,
-            Unit: ingredientUnit,
-        }
-
-        ingredients = append(ingredients, ingredient)
-
-    }
-
-    recipe := Recipe {
-        Name: recipeName,
-        Ingredients: ingredients,
-        Notes: "",
-    }
-
-    fmt.Println(recipe)
-
-    // ash := Trainer{"Ash", 10, "Pallet Town"}
-    insertResult, err := coll.InsertOne(context.TODO(), recipe)
-
-    if err != nil {
-    log.Fatal(err)
-    }
-
-    fmt.Println("Inserted a single document: ", insertResult.InsertedID)
-
-    // filter := bson.D{{"name", "Ash"}}
-
-    // update := bson.D{
-    //     {"$inc", bson.D{
-    //         {"age", 1},
-    //     }},
-    // }
-
-    // updateResult, err := coll.UpdateOne(context.TODO(), filter, update)
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-
-    // fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
-
-	// title := "Back to the Future"
-	// var result bson.M
-	// err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
-	// if err == mongo.ErrNoDocuments {
-	// 	fmt.Printf("No document was found with the title %s\n", title)
-	// 	return
-	// }
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// jsonData, err := json.MarshalIndent(result, "", "    ")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("%s\n", jsonData)
+    text, _ := reader.ReadString('\n')
+    text = strings.Replace(text, "\n", "", -1)
+    return text
 }
+
+
+
+// var coll *mongo.Collection = db.ConnectDB()
+func main() {
+    // recipes := db.GetRecipes()
+    // fmt.Println(recipes)
+    // recipe := db.GetRecipe(coll)
+    // fmt.Println(recipe)
+    // recipes := db.GetRecipes(coll)
+    // fmt.Println(recipes)
+    router := gin.Default()
+    router.GET("/api/recipe", db.GetRecipes)
+    router.GET("/api/recipe/:name", db.GetRecipe)
+    // fmt.Println(recipes)
+    // router.GET("/api/recipes/{id}", GetTodo)
+    // router.POST("/api/recipes", AddTodo)
+    router.Run("localhost:9090")
+}
+
+// func getRecipes(context *gin.Context) {
+//     recipes := db.GetRecipes(coll)
+//     context.IndentedJSON(http.StatusOK, recipes)
+// }
+
+
